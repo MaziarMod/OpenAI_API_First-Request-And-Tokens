299e62bc3639d2b51b55ff5d8502984b9bb19e8c
Maziar <Maziar.mod@gmail.com>
2023-06-06 21:58:40 -0400

    add n.js

---
+++ n.js
@@ -0,0 +1,19 @@
+const { Configuration, OpenAIApi } = require('openai');
+require('dotenv').config();
+
+(async () => {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const completion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: 'write me a poem',
+    max_tokens: 100,
+    n: 3,
+  });
+
+  const completion_text = completion.data.choices[0].text;
+  console.log(completion_text);
+})();
